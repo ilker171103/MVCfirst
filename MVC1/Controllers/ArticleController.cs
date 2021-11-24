e3325b9f082bb9bb5618c3aa3f2b9934868eddf5
ïlker171103 <ilker.2000@abv.bg>
2021-11-24 13:47:03 +0200

    Project upload

---
+++ MVC1/Controllers/ArticleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC1.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult ById(string name)
+        {
+            var viewModel = new DataViewModel();
+            viewModel.ParameterValue = name;
+            this.ViewBag.Name = "Asen";
+            return this.View(viewModel);
+        }
+    }
+}
---
+++ MVC1/Models/DataViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC1.Models
+{
+    public class DataViewModel
+    {
+        public string ParameterValue;
+    }
+}
